4a38a7d44bffb31a682b73611bcae7c6d8faa74b
oguz56785 <oguzyildirim@posta.mu.edu.tr>
2023-04-20 17:39:27 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Oğuz Yıldırım on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
